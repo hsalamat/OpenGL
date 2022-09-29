44f25d1135035ba2f11045a0303608ce01372360
Hooman Salamat <hooman_salamat@yahoo.com>
2022-09-29 19:47:27 -0400

    Wireframe

---
+++ Week4/Week4-1-WireframeDemo.cpp
@@ -0,0 +1,191 @@
+
+///////////////////////////////////////////////////////////////////////
+//
+// Frame-independent movement
+// Measure the time each frame takes
+///////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+#include "stdlib.h"
+#include "time.h"
+#include <GL/glew.h>
+#include <GL/freeglut.h> 
+#include "prepShader.h"
+#include "glm\glm.hpp"
+#include "glm\gtc\matrix_transform.hpp"
+#include <string>
+
+#define X_AXIS glm::vec3(1,0,0)
+#define Y_AXIS glm::vec3(0,1,0)
+#define Z_AXIS glm::vec3(0,0,1)
+#define XY_AXIS glm::vec3(1,1,0)
+#define YZ_AXIS glm::vec3(0,1,1)
+#define XZ_AXIS glm::vec3(1,0,1)
+
+GLuint vao, ibo, points_vbo, colors_vbo, modelID;
+float rotAngle = 0.0f;
+int deltaTime, currentTime, lastTime = 0;
+
+static unsigned int
+program,
+vertexShaderId,
+fragmentShaderId;
+
+GLshort cube_indices[] = {
+	// Front.
+	0, 1, 2,
+	3, 0, 2,
+	// Right.
+	6, 5, 1,
+	6, 2,
+	// Top.
+	3,7,6,
+	3,
+	// left.
+	7,  4, 0,
+	7,
+	// Back.
+	5, 4,
+	// Bottom.
+	1,
+};
+
+GLfloat cube_vertices[] = {
+	-0.9f, -0.9f, 0.9f,		// 0.
+	0.9f, -0.9f, 0.9f,		// 1.
+	0.9f, 0.9f, 0.9f,		// 2.
+	-0.9f, 0.9f, 0.9f,		// 3.
+	-0.9f, -0.9f, -0.9f,	// 4.
+	0.9f, -0.9f, -0.9f,		// 5.
+	0.9f, 0.9f, -0.9f,		// 6.
+	-0.9f, 0.9f, -0.9f,		// 7.
+};
+
+GLfloat colors[] = {
+	1.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+	0.0f, 0.0f, 1.0f,
+	1.0f, 1.0f, 0.0f,
+	1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 1.0f,
+	0.5f, 0.0f, 0.0f,
+	0.0f, 0.5f, 0.0f
+};
+
+void init(void)
+{
+
+	   // Create shader program executable.
+
+	vertexShaderId = setShader((char*)"vertex", (char*)"cube.vert");
+	fragmentShaderId = setShader((char*)"fragment", (char*)"cube.frag");
+	program = glCreateProgram();
+	glAttachShader(program, vertexShaderId);
+	glAttachShader(program, fragmentShaderId);
+	glLinkProgram(program);
+	glUseProgram(program);
+
+	modelID = glGetUniformLocation(program, "model");
+
+	vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), cube_indices, GL_STATIC_DRAW);
+
+	points_vbo = 0;
+	glGenBuffers(1, &points_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);
+
+	colors_vbo = 0;
+	glGenBuffers(1, &colors_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // Can optionally unbind the buffer to avoid modification.
+
+	glBindVertexArray(0); // Can optionally unbind the vertex array to avoid modification.
+
+	//Enable depth test.
+	glEnable(GL_DEPTH_TEST);
+
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CCW);
+	glCullFace(GL_BACK);
+}
+
+//---------------------------------------------------------------------
+//
+// transformModel
+//
+
+void transformObject(glm::vec3 scale, glm::vec3 rotationAxis, float rotationAngle, glm::vec3 translation) {
+	glm::mat4 Model;
+	Model = glm::mat4(1.0f);
+	Model = glm::translate(Model, translation);
+	Model = glm::rotate(Model, glm::radians(rotationAngle), rotationAxis);
+	Model = glm::scale(Model, scale);
+	glUniformMatrix4fv(modelID, 1, GL_FALSE, &Model[0][0]);
+}
+
+//---------------------------------------------------------------------
+//
+// display
+//
+
+void display(void)
+{
+	// Delta time stuff.
+	currentTime = glutGet(GLUT_ELAPSED_TIME); // Gets elapsed time in milliseconds.
+	deltaTime = currentTime - lastTime;
+	lastTime = currentTime;
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBindVertexArray(vao);
+
+	transformObject(glm::vec3(0.4,0.4,0.4), YZ_AXIS, rotAngle -= ((float)1.0 / (float)60.0f * deltaTime), 
+		glm::vec3(0.0f, 0.0f, 0.0f));
+
+	//Ordering the GPU to start the pipeline
+	glDrawElements(GL_LINE_LOOP, 22, GL_UNSIGNED_SHORT, 0); // Try GL_LINE_STRIP too!
+
+	glBindVertexArray(0); // Can optionally unbind the vertex array to avoid modification.
+
+	glutSwapBuffers(); // Now for a potentially smoother render.
+}
+
+void idle()
+{
+	glutPostRedisplay();
+}
+
+//---------------------------------------------------------------------
+//
+// main
+//
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize(1024, 1024);
+	glutCreateWindow("Hello World");
+
+	glewInit();	//Initializes the glew and prepares the drawing pipeline.
+
+	init();
+
+	glutDisplayFunc(display);
+
+	glutIdleFunc(idle);
+
+	glutMainLoop();
+}
